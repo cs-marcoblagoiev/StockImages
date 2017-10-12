6ea4e113d824c51044b9d7b6c524555b093e3df5
Marco <marcoblagoiev@yahoo.com>
2017-10-12 21:20:56 +0300

    added more images + pagination

--- src/AppBundle/Controller/GalleryController.php
+++ src/AppBundle/Controller/GalleryController.php
@@ -10,20 +10,31 @@ class GalleryController extends Controller
 {
     /**
      * @Route("/gallery", name="gallery")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
+        $images = [
+            'aed58e014ce4f61d66a2add5302557f0.jpg',
+            'animals-rabbits.jpg',
+            'free-stock-photosnature-squirrels.jpg',
+            'pexels-photo-312121.jpeg',
+            'trees-land-stock.jpg',
+            'videoblocks-sunny.png',
+            'wildlife-animals.png',
+        ];
+
+        $paginator  = $this->get('knp_paginator');
+        $pagination = $paginator->paginate(
+            $images, /* query NOT result */
+            $request->query->getInt('page', 1)/*page number*/,
+            4/*limit per page*/
+        );
+
+
         return $this->render('gallery/index.html.twig', [
-            'images' => [
-                'pexels-photo-312121.jpeg',
-                'pexels-photo-312121.jpeg',
-                'pexels-photo-312121.jpeg',
-                'pexels-photo-312121.jpeg',
-                'pexels-photo-312121.jpeg',
-                'pexels-photo-312121.jpeg',
-                'pexels-photo-312121.jpeg',
-                'pexels-photo-312121.jpeg',
-            ]
+            'images' => $pagination,
         ]);
     }
 }
