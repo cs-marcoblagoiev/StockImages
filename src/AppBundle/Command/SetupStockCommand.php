d8f76e08ad8c8d418364e1bbe80b4d37687f8717
Marco <marco.blagoiev@cobaltsign.com>
2017-10-30 21:18:43 +0200

    changed the command to iterate trough all the pictures in the img folder

--- src/AppBundle/Command/SetupStockCommand.php
+++ src/AppBundle/Command/SetupStockCommand.php
@@ -2,12 +2,29 @@
 
 namespace AppBundle\Command;
 
+use AppBundle\Entity\Stock;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class SetupStockCommand extends Command
 {
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+    private $rootDir;
+
+    public function __construct($rootDir, EntityManagerInterface $em)
+    {
+        parent::__construct();
+
+        $this->rootDir = $rootDir;
+        $this->em = $em;
+    }
+
+
     protected function configure()
     {
         $this
@@ -18,6 +35,22 @@ class SetupStockCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $stockImages = glob($this->rootDir . '/../web/images/*.*');
+
+        foreach ($stockImages as $image){
+
+            $img = (new Stock())
+                ->setFilename($image)
+                ->setSlug($image)
+                ->setWidth(1920)
+                ->setHeight(1080)
+            ;
+
+            $this->em->persist($img);
+        }
+
+        $this->em->flush();
+
         $output->writeln('Command result.');
     }
 
