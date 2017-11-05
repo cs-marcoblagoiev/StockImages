3f0ea82ede47aecb83d80d567ce63b44cc53a317
Marco <marco.blagoiev@cobaltsign.com>
2017-11-05 19:26:03 +0200

    improved the command

--- src/AppBundle/Command/SetupStockCommand.php
+++ src/AppBundle/Command/SetupStockCommand.php
@@ -5,8 +5,10 @@ namespace AppBundle\Command;
 use AppBundle\Entity\Stock;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 class SetupStockCommand extends Command
 {
@@ -35,23 +37,54 @@ class SetupStockCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $io = new SymfonyStyle($input, $output);
+
         $stockImages = glob($this->rootDir . '/../web/images/*.*');
+        $imageCount = count($stockImages);
+
+        $io->title('Importing Images');
+        $io->progressStart($imageCount);
+
+        $fileNames = [];
 
         foreach ($stockImages as $image){
 
+            [
+                'basename' => $filename,
+                'filename' => $slug,
+            ] = pathinfo($image);
+
+            [
+                0 => $width,
+                1 => $height,
+            ] = getimagesize($image);
+
             $img = (new Stock())
-                ->setFilename($image)
-                ->setSlug($image)
-                ->setWidth(1920)
-                ->setHeight(1080)
+                ->setFilename($filename)
+                ->setSlug($slug)
+                ->setWidth($width)
+                ->setHeight($height)
             ;
 
             $this->em->persist($img);
+
+            $io->progressAdvance();
+
+            $fileNames[] = [$filename];
         }
 
         $this->em->flush();
 
-        $output->writeln('Command result.');
+        $io->progressFinish();
+
+        $table = new Table($output);
+        $table
+            ->setHeaders(['Filename'])
+            ->setRows($fileNames);
+
+        $table->render();
+
+        $io->success(sprintf('Added %d images', $imageCount));
     }
 
 }
